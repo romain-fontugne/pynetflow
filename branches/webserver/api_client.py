8deedf25e63b4ad87f44e6de96a22e2f46b75fe6
choonho.son <choonho.son@39429416-dac9-2948-6aab-76f2b7f4db6d>
2011-02-18 09:52:50 +0000

    Fix Option parser

--- branches/webserver/api_client.py
+++ branches/webserver/api_client.py
@@ -2,14 +2,16 @@ import socket
 import sys
 import time
 
-HOST, PORT = "localhost", 9000
+HOST, PORT = "localhost", 10200
 
-data = "show %s %s -1 1000" % (sys.argv[1], (long(time.time()) - 3600) )
+data = "exit"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.connect( (HOST,PORT) )
-sock.send(data+"\n")
+cmd = sys.argv[1]
+print "Cmd: ", cmd
+sock.send(cmd)
 
 recv = sock.recv(1024)
 print recv
